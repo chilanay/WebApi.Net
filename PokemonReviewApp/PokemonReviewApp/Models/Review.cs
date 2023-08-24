f88991f4f2165ebad9f11274d9859e62d2d5bc3f
Chilanay <chilanay.hajisoy@gmail.com>
2023-08-24 12:49:35 +0400

    Files added

---
+++ PokemonReviewApp/PokemonReviewApp/Models/Review.cs
@@ -0,0 +1,10 @@
+﻿namespace PokemonReviewApp.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public Reviewer Reviewer { get; set; }
+        public Pokemon Pokemon { get; set; }
+    }
+}
